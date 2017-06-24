0b5daa1c1bffe95970e55a9806995b604057aefc
Руслан Идрисов <gaarson@localhost.local>
2017-06-24 09:01:38 +0300

    заголовки

--- client/components/home/header.js
+++ client/components/home/header.js
@@ -3,10 +3,10 @@ import React from 'react';
 export const Header = props => 
   <nav id="nav">
 		<ul className="navbar">
-			<li className="navbar-itm"><a href="#one" className="navbar-link">What I Do</a></li>
-			<li className="navbar-itm"><a href="#two" className="navbar-link">Who I Am</a></li>
-			<li className="navbar-itm"><a href="#three" className="navbar-link">My Work</a></li>
-			<li className="navbar-itm"><a href="#four" className="navbar-link">Contact</a></li>
+			<li className="navbar-itm"><a href="#one" className="navbar-link">Чем я занимаюсь</a></li>
+			<li className="navbar-itm"><a href="#two" className="navbar-link">Обо мне</a></li>
+			<li className="navbar-itm"><a href="#three" className="navbar-link">Работы</a></li>
+			<li className="navbar-itm"><a href="#four" className="navbar-link">Контакты</a></li>
 		</ul>
-		<h1 className="logo">User Name</h1>
+		<h1 className="logo">Гачегова Арина</h1>
 	</nav>
